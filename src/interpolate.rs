c6ced407a458dc1ac2ea08a3dd8b1300f8679d72
Sequal32 <connor0530@gmail.com>
2020-09-02 12:19:34 -0400

    fix interpolation logic for pitch + bank fix timing logic

--- src/interpolate.rs
+++ src/interpolate.rs
@@ -4,7 +4,7 @@ use std::time::{Instant};
 
 struct Record {
     data: IndexMap<String, StructDataTypes>,
-    time: Instant
+    time: f64
 }
 
 pub struct InterpolateStruct {
@@ -12,8 +12,12 @@ pub struct InterpolateStruct {
     latest: Option<Record>,
     at_latest: Option<Record>,
     current: Option<Record>,
-    special_floats: Vec<String>,
+    instant_at_latest: Instant,
     interpolation_time: f64,
+
+    special_floats_regular: Vec<String>,
+    special_floats_wrap180: Vec<String>,
+    special_floats_wrap90: Vec<String>,
 
     add_alpha: f64,
     next_add_alpha: f64
@@ -21,24 +25,38 @@ pub struct InterpolateStruct {
 
 impl Default for InterpolateStruct {
     fn default() -> Self {
-        Self {last: None, latest: None, current: None, at_latest: None, special_floats: vec![], interpolation_time: 0.0, add_alpha: 0.0, next_add_alpha: 0.0}
+        Self {
+            last: None, 
+            latest: None, 
+            current: None, 
+            at_latest: None, 
+            instant_at_latest: std::time::Instant::now(), 
+            interpolation_time: 0.0, 
+            add_alpha: 0.0, 
+            next_add_alpha: 0.0,
+
+            special_floats_regular: vec![],
+            special_floats_wrap90: vec![],
+            special_floats_wrap180: vec![],
+        }
     }
 }
 
-fn get_time() -> Instant {
-    return std::time::Instant::now();
+fn get_time() -> f64 {
+    return std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs_f64();
 }
 
 impl InterpolateStruct {
     pub fn new() -> Self {return Self::default()}
 
-    pub fn record_latest(&mut self, data: IndexMap<String, StructDataTypes>) {
+    pub fn record_latest(&mut self, data: IndexMap<String, StructDataTypes>, time: f64) {
         self.last = self.latest.take();
         self.at_latest = self.current.take();
-        self.latest = Some(Record {data, time: get_time()});
+        self.latest = Some(Record {data, time: time});
+        self.instant_at_latest = std::time::Instant::now();
 
         if self.last.is_some() {
-            self.interpolation_time = self.latest.as_ref().unwrap().time.duration_since(self.last.as_ref().unwrap().time).as_secs_f64();
+            self.interpolation_time = self.latest.as_ref().unwrap().time-self.last.as_ref().unwrap().time;
             self.add_alpha = self.next_add_alpha;
         }
     }
@@ -47,26 +65,34 @@ impl InterpolateStruct {
         self.current = Some(Record {data, time: get_time()});
     }
 
-    pub fn add_special_floats(&mut self, data: &mut Vec<String>) {
-        self.special_floats.append(data);
+    pub fn add_special_floats_regular(&mut self, data: &mut Vec<String>) {
+        self.special_floats_regular.append(data);
     }
 
-    pub fn get_last_position_time(&self) -> Option<Instant> {
+    pub fn add_special_floats_wrap90(&mut self, data: &mut Vec<String>) {
+        self.special_floats_wrap90.append(data);
+    }
+
+    pub fn add_special_floats_wrap180(&mut self, data: &mut Vec<String>) {
+        self.special_floats_wrap180.append(data);
+    }
+
+    pub fn get_time_since_last_position(&self) -> Option<f64> {
         if let Some(s) = &self.latest {
-            return Some(s.time);
+            return Some(self.instant_at_latest.elapsed().as_secs_f64());
         }
         return None;
     }
 
     pub fn interpolate(&mut self) -> Option<IndexMap<String, StructDataTypes>> {
-        if self.latest.is_none() || self.at_latest.is_none() {return None}
+        if self.latest.is_none() || self.at_latest.is_none() || self.interpolation_time == 0.0 {return None}
 
         let mut interpolated = IndexMap::<String, StructDataTypes>::new();
 
         let current = self.at_latest.as_ref().unwrap();
         let latest = self.latest.as_ref().unwrap();
 
-        let alpha = latest.time.elapsed().as_secs_f64()/self.interpolation_time + self.add_alpha;
+        let alpha = self.instant_at_latest.elapsed().as_secs_f64()/self.interpolation_time + self.add_alpha;
         if alpha > 10.0 {return None}
         
         // Account for lag (prevent plane moving backward)
@@ -81,9 +107,14 @@ impl InterpolateStruct {
                 StructDataTypes::F64(n) => {
                     if let Some(StructDataTypes::F64(current_value)) = current.data.get(key) {
                         let value: f64;
-                        if self.special_floats.contains(key) {
+                        if self.special_floats_regular.contains(key) {
                             value = interpolate_f64_degrees(*current_value, *n, alpha);   
-                        } else {
+                        } else if self.special_floats_wrap90.contains(key) {
+                            value = interpolate_f64_degrees_90(*current_value, *n, alpha);   
+                        } else if self.special_floats_wrap180.contains(key) {
+                            value = interpolate_f64_degrees_180(*current_value, *n, alpha);   
+                        }
+                        else {
                             value = interpolate_f64(*current_value, *n, alpha);
                         }
                         interpolated.insert(key.to_string(), StructDataTypes::F64(value));
@@ -104,15 +135,29 @@ pub fn interpolate_f64(from: f64, to: f64, alpha: f64) -> f64 {
 }
 
 pub fn interpolate_f64_degrees(from: f64, to: f64, alpha: f64) -> f64 {
-    // turning left
-    if from < 180.0 && to > 180.0 {
-        (from + alpha * -(360.0 - to + from)) % 360.0
-    } else if from > 180.0 && to < 180.0 {
-        (from + alpha * (360.0 - from + to)) % 360.0
+    // If we need to wrap
+    if (from - to).abs() > 180.0 {
+        // Turning right
+        if from < 180.0 && to > 180.0 {
+            let from = from + 360.0;
+            (from + alpha * -(from - to)) % 360.0
+        }
+        // Turning left
+        else {
+            (from + alpha * (to + 360.0 - from)) % 360.0
+        }
     }
     else {
-        return interpolate_f64(from, to, alpha);
+        interpolate_f64(from, to, alpha)
     }
+}
+
+pub fn interpolate_f64_degrees_180(from: f64, to: f64, alpha: f64) -> f64 {
+    interpolate_f64_degrees(from + 180.0, to + 180.0, alpha) - 180.0
+}
+
+pub fn interpolate_f64_degrees_90(from: f64, to: f64, alpha: f64) -> f64 {
+    interpolate_f64_degrees(from + 270.0, to + 270.0, alpha) - 270.0
 }
 
 #[cfg(test)]
@@ -130,5 +175,11 @@ mod tests {
         assert_eq!(interpolate_f64_degrees(358.0, 10.0, 0.5) as i32, 4);
         assert_eq!(interpolate_f64_degrees(10.0, 355.0, 0.5) as i32, 2);
         assert_eq!(interpolate_f64_degrees(358.0, 358.0, 0.5) as i32, 358);
+    }
+
+    #[test]
+    fn test_wrap90() {
+        assert_eq!(interpolate_f64_degrees_180(179.0, -179.0, 0.8) as i32, -179);
+        assert_eq!(interpolate_f64_degrees_180(-30.0, 30.0, 0.5) as i32, 0);
     }
 }
