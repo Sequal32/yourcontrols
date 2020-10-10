3f11bde76f4e4907ea7b44ca03b390d7e4425734
Sequal32 <connor0530@gmail.com>
2020-10-10 18:29:16 -0400

    alignment + fix active config

--- web/aircraft.js
+++ web/aircraft.js
@@ -8,13 +8,15 @@ aircraftList.addAircraft = function(aircraftName) {
 
     newButton.onclick = function() {
         invoke({"type":"load_aircraft", "name": aircraftName})
-        newButton.classList.add("active")
-    }
 
-    if (aircraftList.activeSelection) {
-        aircraftList.activeSelection.classList.remove("active")
+        if (aircraftList.activeSelection) {
+            aircraftList.activeSelection.classList.remove("active")
+        }
+
+        newButton.classList.add("active")
+        
+        aircraftList.activeSelection = newButton
     }
-    aircraftList.activeSelection = newButton
 
     aircraftList.appendChild(newButton)
 }
