213520c4e3f7749fcb068ad313df98cde3456e67
Sequal32 <connor0530@gmail.com>
2020-10-26 23:06:51 -0400

    add app icon

---
+++ build.rs
@@ -0,0 +1,9 @@
+use winres;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
