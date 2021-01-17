5e66c5bfff02e6b33e0018b2c573a04de3bc28c4
Sequal32 <connor0530@gmail.com>
2021-01-17 10:22:49 -0500

    placeholder text, only force if not observer

--- src/main.rs
+++ src/main.rs
@@ -431,7 +431,7 @@ fn main() {
 
             // Handle specific program triggered actions
             if definitions.control_transfer_requested {
-                if !control.has_control() {
+                if !control.has_control() && !observing {
                     control.take_control();
                     client.take_control(clients.get_client_in_control().unwrap().clone());
                 }
