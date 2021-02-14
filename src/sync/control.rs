98fc3c1db8cfa85a0349ecc1c425862b454ec768
Sequal32 <connor0530@gmail.com>
2021-02-14 01:25:10 -0500

    add back freeze

--- src/sync/control.rs
+++ src/sync/control.rs
@@ -16,6 +16,7 @@ impl Control {
     pub fn do_transfer(&mut self, conn: &SimConnector) {
         conn.transmit_client_event(1, 1000, !self.has_control as u32, 5, 0);
         conn.transmit_client_event(1, 1001, !self.has_control as u32, 5, 0);
+        conn.transmit_client_event(1, 1002, !self.has_control as u32, 5, 0);
     }
 
     pub fn take_control(&mut self, conn: &SimConnector, gauge_communicator: &GaugeCommunicator) {
@@ -36,5 +37,6 @@ impl Control {
     pub fn on_connected(&mut self, conn: &SimConnector) {
         conn.map_client_event_to_sim_event(1000, "FREEZE_LATITUDE_LONGITUDE_SET");
         conn.map_client_event_to_sim_event(1001, "FREEZE_ALTITUDE_SET");
+        conn.map_client_event_to_sim_event(1002, "FREEZE_ATTITUDE_SET");
     }
 }
