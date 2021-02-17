83a70307ee21a6860a408da21b51df0b0408d077
Sequal32 <connor0530@gmail.com>
2021-02-17 04:12:40 -0500

    readd build script to correct location

--- src/yourcontrols/build.rs
+++ src/yourcontrols/build.rs
@@ -3,7 +3,7 @@ use winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/icon.ico");
+        res.set_icon("../../assets/icon.ico");
         res.compile().unwrap();
     }
 }
