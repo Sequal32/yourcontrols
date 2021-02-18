1a8c7f419300e195b57c42fc9746413607af965a
Sequal32 <connor0530@gmail.com>
2021-02-18 00:24:10 -0500

    disconnect only for established connections, need to use timeout

--- src/yourcontrols-net/src/messages.rs
+++ src/yourcontrols-net/src/messages.rs
@@ -87,7 +87,7 @@ impl SenderReceiver {
         // Receive packet
         let packet = match self.receiver.try_recv()? {
             SocketEvent::Packet(packet) => packet,
-            SocketEvent::Disconnect(addr) => {return Ok(Message::ConnectionClosed(addr))},
+            SocketEvent::Timeout(addr) => {return Ok(Message::ConnectionClosed(addr))},
             SocketEvent::Metrics(addr, metrics) => {return Ok(Message::Metrics(addr, metrics))},
             _ => {return Err(Error::NotProcessed)}
         };
