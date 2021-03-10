3fc21df171bf388b0ec637c4770ee0187d1b9fb8
Sequal32 <connor0530@gmail.com>
2021-03-10 17:28:42 -0500

    ✅ condition test

--- src/yourcontrols-gauge/src/sync/condition.rs
+++ src/yourcontrols-gauge/src/sync/condition.rs
@@ -3,6 +3,7 @@ use crate::util::DatumValue;
 
 /// If var is specified, the condition will be applied on the value of the var when `is_satisfied` is called.
 /// Otherwise, the condition will be applied on the `incoming_value`.
+#[derive(Default)]
 pub struct Condition {
     var: Option<RcVariable>,
     equals: Option<DatumValue>,
@@ -19,7 +20,33 @@ impl Condition {
             .map_or(incoming_value, |x| x.borrow().get());
 
         return self.equals.map_or(true, |x| x == operating_on_value)
-            && self.less_than.map_or(true, |x| x < operating_on_value)
-            && self.greater_than.map_or(true, |x| x > operating_on_value);
+            && self.less_than.map_or(true, |x| operating_on_value < x)
+            && self.greater_than.map_or(true, |x| operating_on_value > x);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Condition;
+
+    #[test]
+    fn test_incoming_value_condition() {
+        let condition = Condition {
+            less_than: Some(10.0),
+            greater_than: Some(5.0),
+            ..Default::default()
+        };
+
+        assert!(condition.is_satisfied(7.5));
+        assert!(!condition.is_satisfied(3.5));
+        assert!(!condition.is_satisfied(15.5));
+
+        let condition = Condition {
+            equals: Some(5.0),
+            ..Default::default()
+        };
+
+        assert!(condition.is_satisfied(5.0));
+        assert!(!condition.is_satisfied(2.3));
     }
 }
