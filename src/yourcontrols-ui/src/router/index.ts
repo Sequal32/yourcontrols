87ba26efff4e3123c2fbd811e7c0559e55a74ebf
SafeShows <nils3nils@gmail.com>
2021-03-14 00:13:26 +0200

    ♻️ improved router file

--- src/yourcontrols-ui/src/router/index.ts
+++ src/yourcontrols-ui/src/router/index.ts
@@ -1,23 +1,17 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+import Loading from "../views/Loading.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: About
+    path: "/loading",
+    name: "loading",
+    component: Loading
   }
 ];
 
