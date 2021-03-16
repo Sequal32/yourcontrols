87ff036eb78b6a364c09092f4fcd1bb5e98b08fc
SafeShows <nils3nils@gmail.com>
2021-03-17 01:18:00 +0200

    🚧 WIP: aircraft screen/page

---
+++ src/yourcontrols-ui/src/views/Aircraft.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="aircraft-view">
+    <router-link :to="{name:'main'}">
+      &#60;-  back
+    </router-link>
+    <br>
+    aircraft page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.aircraft-view {
+  padding: 20px;
+  a {
+    text-decoration: none;
+  }
+}
+</style>
