a66ceb602cd66c59f9f9b424b5adbc8aa4240ae4
SafeShows <nils3nils@gmail.com>
2021-03-17 01:15:52 +0200

    💄 home and changelog screen/page

---
+++ src/yourcontrols-ui/src/views/Changelog.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="changelog-view">
+    <router-link :to="{name:'main'}">
+      &#60;-  back
+    </router-link>
+    <div class="title">Chanelog</div>
+    <div class="sub-title">Version {{version}}</div>
+    <br>
+    <div class="body" v-html="text" @click.prevent="doNothing()" v-if="!loading"/>
+    <div v-else>
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import marked from 'marked'
+import axios from 'axios'
+
+interface Data {
+  version: string | null,
+  text: string,
+  loading: boolean
+}
+
+export default Vue.extend({
+  data:(): Data => {
+    return {
+      version: window.localStorage.getItem("version"),
+      text: "",
+      loading: true
+    }
+  },
+  mounted() {
+    axios.get("https://api.github.com/repos/Sequal32/yourcontrols/releases/latest")
+    .then(
+      response => {
+        this.text = marked(response.data.body)
+        this.loading = false
+      }
+    )
+  },
+  methods: {
+    doNothing() {
+      return;
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.changelog-view {
+  padding: 20px;
+  .title {
+    font: normal normal bold 32px/43px Segoe UI;
+    text-align: center;
+  }
+  .sub-title {
+    font: normal normal bold 19px/26px Segoe UI;
+    text-align: center;
+  }
+  a {
+    text-decoration: none;
+  }
+  .body{
+    font: normal normal normal 13px/17px Segoe UI;
+    a {
+      color: #fff;
+      cursor: auto;
+    }
+    h2 {
+      margin: 15px 0px;
+      color: #fff;
+      font: normal normal bold 15px/20px Segoe UI;
+    }
+    ul {
+      li {
+        margin-left: 30px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/yourcontrols-ui/src/views/Main.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="main">
+    <img alt="YourControls" src="../assets/logo.png"/>
+    <div class="version">
+      Version {{version}}
+    </div>
+    <div class="selector">
+      <router-link class="button aircraft" :to="{name:'aircraft'}">
+        Aircraft
+      </router-link>
+      <router-link class="button changelog" :to="{name:'changelog'}">
+        Changelog
+      </router-link>
+      <router-link class="button network" :to="{name:'networkTest'}">
+        Test Network
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  computed: {
+    version() {
+      return window.localStorage.getItem("version")
+    }
+  },
+});
+</script>
+
+<style lang="scss">
+.main {
+  text-align: center;
+  img {
+    margin: 15px;
+    width: 75%; 
+  }
+  .version{
+    font: normal normal 300 19px/26px Segoe UI;
+  }
+  .selector{
+    display: flex;
+    width: 75%;
+    margin: auto;
+    margin-top: 6.5rem;
+    justify-content: space-around;
+    .button{
+      height: 92px;
+      width: 92px;
+      background-color: #00404C;
+      text-align: center;
+      padding: 8px;
+      border-radius: 1rem;
+      display: flex;
+      justify-content: center;
+      align-content: flex-end;
+      font: normal normal bold 13px/17px Segoe UI;
+      letter-spacing: 0px;
+      text-decoration: none;
+      &.aircraft {
+        background-image: url("../assets/plane.png");
+        background-size: 60%;
+        background-repeat: no-repeat;
+        background-position: 50%;
+        background-position-y: 70%;
+      }
+      &.changelog {
+        background-image: url("../assets/changelog.png");
+        background-size: 50%;
+        background-repeat: no-repeat;
+        background-position: 50%;
+        background-position-y: 70%;
+      }
+      &.network {
+        background-image: url("../assets/test_network.png");
+        background-size: 50%;
+        background-repeat: no-repeat;
+        background-position: 50%;
+        background-position-y: 70%;
+      }
+      &:hover {
+        background-color: #005566;
+        cursor: pointer;
+      }
+      &.min{
+        width: 99px;
+        height: 28px;
+        font: normal normal bold 17px/22px Segoe UI;
+      }
+    }
+  }
+}
+</style>
