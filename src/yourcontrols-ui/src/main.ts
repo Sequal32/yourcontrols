f714f498bdf5940623d2f7d7d55dd52f0bd006ec
SafeShows <nils3nils@gmail.com>
2021-03-17 16:12:52 +0200

    ➕ added font awesome

--- src/yourcontrols-ui/src/main.ts
+++ src/yourcontrols-ui/src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 Vue.config.productionTip = false;
 
