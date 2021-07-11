70fb2dd43c098fb1066d201ab3e2e80e246285d8
Sequal32 <connor0530@gmail.com>
2021-07-11 02:00:49 -0400

    refactor(core): split event handlers into own functions

--- src/yourcontrols/src-tauri/src/program.rs
+++ src/yourcontrols/src-tauri/src/program.rs
@@ -1,6 +1,7 @@
 use anyhow::{ensure, Result};
+use std::collections::HashMap;
 use std::fs::read_dir;
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
 use std::path::{Path, PathBuf};
 use yourcontrols_net::MainPayloads;
 
@@ -12,7 +13,7 @@ use crate::ui::cmd::UiEvents;
 use crate::ui::Ui;
 
 use yourcontrols_definitions::DefinitionsParser;
-use yourcontrols_types::{ChangedDatum, Payloads, WatchPeriod};
+use yourcontrols_types::{ChangedDatum, ControlSurfaces, Payloads, WatchPeriod};
 
 const DEFINITIONS_PATH: &str = "definitions";
 
@@ -37,6 +38,7 @@ impl<U: Ui> Program<U> {
         }
     }
 
+    /// Reads definitions from the filesystem and loads them into the definitions parser
     pub fn load_definitions(&mut self, target_definition_sub_path: impl AsRef<Path>) -> Result<()> {
         let mut core_path = PathBuf::from(DEFINITIONS_PATH);
         core_path.push("templates");
@@ -77,6 +79,7 @@ impl<U: Ui> Program<U> {
         return true;
     }
 
+    /// Polls and processes events from the gauge
     pub fn process_sim_events(&mut self) -> Result<()> {
         let msg = match self.simulator.poll() {
             Ok(m) => m,
@@ -85,21 +88,7 @@ impl<U: Ui> Program<U> {
 
         match msg {
             Payloads::VariableChange { changed } => {
-                let (unreliable, reliable): (Vec<ChangedDatum>, Vec<ChangedDatum>) =
-                    changed.into_iter().partition(|a| {
-                        self.definitions_parser
-                            .get_parsed_datums()
-                            .get(a.key as usize)
-                            .map(|x| x.watch_period == Some(WatchPeriod::Frame))
-                            .unwrap_or(false)
-                    });
-
-                self.network.send_update(
-                    *self.clients.self_client().id(),
-                    0.0,
-                    unreliable,
-                    reliable,
-                )?;
+                self.sim_on_vars_changed(changed)?;
             }
             _ => {}
         };
@@ -107,6 +96,36 @@ impl<U: Ui> Program<U> {
         Ok(())
     }
 
+    /// Returns true if the datum is being updated every frame
+    fn datum_is_frame(&self, datum: &ChangedDatum) -> bool {
+        self.definitions_parser
+            .get_parsed_datums()
+            .get(datum.key)
+            .map(|x| x.watch_period == Some(WatchPeriod::Frame))
+            .unwrap_or(false)
+    }
+
+    /// Splits a vector of ChangedDatum into two based on whether its updating every frame
+    fn split_reliable_unreliable(
+        &self,
+        changed: Vec<ChangedDatum>,
+    ) -> (Vec<ChangedDatum>, Vec<ChangedDatum>) {
+        changed
+            .into_iter()
+            .partition(|datum| self.datum_is_frame(datum))
+    }
+
+    /// Sends updated vars across the network
+    fn sim_on_vars_changed(&mut self, changed: Vec<ChangedDatum>) -> Result<()> {
+        let (unreliable, reliable) = self.split_reliable_unreliable(changed);
+
+        self.network
+            .send_update(*self.clients.self_client().id(), 0.0, unreliable, reliable)?;
+
+        Ok(())
+    }
+
+    /// Polls and processes all network events
     pub fn process_network_events(&mut self) -> Result<()> {
         let events = match self.network.step() {
             Ok(e) => e,
@@ -118,11 +137,7 @@ impl<U: Ui> Program<U> {
             match event {
                 // Session Events
                 NetworkEvent::Payload(MainPayloads::Welcome { client_id, name }) => {
-                    ensure!(
-                        *self.clients.self_client().name() == name,
-                        "Bad server Welcome."
-                    );
-                    self.clients.self_client().set_id(client_id);
+                    self.on_welcome_received(client_id, name)?
                 }
                 NetworkEvent::Payload(MainPayloads::MakeHost { client_id }) => {
                     self.clients.set_host(client_id);
@@ -133,27 +148,19 @@ impl<U: Ui> Program<U> {
                     is_observer,
                     name,
                 }) => {
-                    self.clients.add_client(id, name, is_observer);
-
-                    if is_host {
-                        self.clients.set_host(id);
-                    }
+                    self.on_client_added_received(id, name, is_observer, is_host);
                 }
                 NetworkEvent::Payload(MainPayloads::ClientRemoved { client_id }) => {
-                    self.clients.remove_client(&client_id);
+                    self.on_client_removed_received(client_id);
+                }
+                NetworkEvent::Payload(MainPayloads::ControlDelegations { delegations }) => {
+                    self.on_delegations_received(delegations);
                 }
                 NetworkEvent::SessionReceived { session_id } => {
-                    self.ui.send_message(UiEvents::LobbyInfo {
-                        session_code: Some(session_id),
-                        server_ip: None,
-                        clients: None,
-                    });
+                    self.on_session_received(session_id);
                 }
                 NetworkEvent::Connected => {
-                    self.ui.send_message(UiEvents::Connected);
-                    self.network.send_payload_to_server(MainPayloads::Name {
-                        name: self.clients.self_client().name().clone(),
-                    })?;
+                    self.on_connected_received()?;
                 }
                 // Game Events
                 NetworkEvent::Payload(MainPayloads::Update {
@@ -162,32 +169,7 @@ impl<U: Ui> Program<U> {
                     time,
                     ..
                 }) => {
-                    let should_allow_update = self
-                        .clients
-                        .get_client(&client_id)
-                        .map(|c| !c.is_observer())
-                        .unwrap_or(false);
-
-                    let delegated_controls =
-                        self.clients.get_control_delegations_for_client(&client_id);
-
-                    let definitions_match_client_delegations = changed.iter().all(|datum| {
-                        self.definitions_parser
-                            .get_meta_data_for(&datum.key)
-                            .and_then(|data| {
-                                data.control_surface
-                                    .as_ref()
-                                    .map(|x| delegated_controls.contains(&x))
-                            })
-                            .unwrap_or(true)
-                    });
-
-                    if should_allow_update && definitions_match_client_delegations {
-                        self.simulator.send_message(Payloads::SendIncomingValues {
-                            data: changed,
-                            time, // TODO: pre-process time
-                        });
-                    }
+                    self.on_update_received(client_id, changed, time);
                 }
 
                 _ => {}
@@ -197,6 +179,92 @@ impl<U: Ui> Program<U> {
         Ok(())
     }
 
+    /// Verifies the server sent back a valid name and stores the assigned client_id
+    fn on_welcome_received(&mut self, client_id: u32, name: String) -> Result<()> {
+        ensure!(
+            *self.clients.self_client().name() == name,
+            "Bad server Welcome."
+        );
+
+        self.clients.self_client().set_id(client_id);
+
+        Ok(())
+    }
+
+    /// Stores the added client's information and notifies the UI
+    fn on_client_added_received(
+        &mut self,
+        id: u32,
+        name: String,
+        is_observer: bool,
+        is_host: bool,
+    ) {
+        self.clients.add_client(id, name, is_observer);
+        if is_host {
+            self.clients.set_host(id);
+        }
+    }
+
+    /// Removes the client's information and notifies the UI
+    fn on_client_removed_received(&mut self, client_id: u32) {
+        self.clients.remove_client(&client_id);
+    }
+
+    /// Stores the new control delegations
+    fn on_delegations_received(&mut self, delegations: HashMap<ControlSurfaces, u32>) {
+        self.clients.set_control_delegations(delegations);
+    }
+
+    /// Notifies the UI of the session_id
+    fn on_session_received(&mut self, session_id: String) {
+        self.ui.send_message(UiEvents::LobbyInfo {
+            session_code: Some(session_id),
+            server_ip: None,
+            clients: None,
+        });
+    }
+
+    /// Notifies the UI we sucessfully established a connection and sends our requested name to the server
+    fn on_connected_received(&mut self) -> Result<(), anyhow::Error> {
+        self.ui.send_message(UiEvents::Connected);
+
+        self.network.send_payload_to_server(MainPayloads::Name {
+            name: self.clients.self_client().name().clone(),
+        })?;
+
+        Ok(())
+    }
+
+    /// Verifies the client is sending legit data with their current permissions and sends the update to the gauge
+    fn on_update_received(&mut self, client_id: u32, changed: Vec<ChangedDatum>, time: f64) {
+        let should_allow_update = self
+            .clients
+            .get_client(&client_id)
+            .map(|c| !c.is_observer())
+            .unwrap_or(false);
+
+        let delegated_controls = self.clients.get_control_delegations_for_client(&client_id);
+
+        let definitions_match_client_delegations = changed.iter().all(|datum| {
+            self.definitions_parser
+                .get_meta_data_for(&datum.key)
+                .and_then(|data| {
+                    data.control_surface
+                        .as_ref()
+                        .map(|x| delegated_controls.contains(&x))
+                })
+                .unwrap_or(true)
+        });
+
+        if should_allow_update && definitions_match_client_delegations {
+            self.simulator.send_message(Payloads::SendIncomingValues {
+                data: changed,
+                time, // TODO: pre-process time
+            });
+        }
+    }
+
+    /// Polls the UI for user-events and processes them
     pub fn process_ui_events(&mut self) -> Result<()> {
         let event = match self.ui.next_event() {
             Some(e) => e,
@@ -205,18 +273,7 @@ impl<U: Ui> Program<U> {
 
         match event {
             UiEvents::UiReady => {
-                self.ui.send_message(UiEvents::StartUpText {
-                    text: "Starting...".to_string(),
-                });
-
-                // self.definitions_updater.fetch_data().expect("OK"); TODO: update repository then uncomment
-
-                self.ui.send_message(UiEvents::InitData {
-                    version: std::env::var("CARGO_PKG_VERSION").unwrap(),
-                    aircraft: self.definitions_updater.get_all_aircraft_info(),
-                });
-
-                self.ui.send_message(UiEvents::LoadingComplete);
+                self.ui_on_ready();
             }
             UiEvents::InstallAircraft { names } => {}
             UiEvents::TestNetwork { port } => {}
@@ -226,21 +283,10 @@ impl<U: Ui> Program<U> {
                 server_ip,
                 username,
             } => {
-                self.clients.self_client().set_name(username);
-
-                if let (Some(port), Some(ip)) = (port, server_ip) {
-                    if let Ok(addr) = format!("{}:{}", ip, port).parse::<SocketAddr>() {
-                        self.network.connect_to_address(addr)?
-                    }
-                } else if let Some(session_code) = session_code {
-                }
+                self.ui_on_join_request(username, port, server_ip, session_code)?;
             }
             UiEvents::Host { port, username } => {
-                if let Some(port) = port {
-                    self.network.start_direct(port)?;
-                } else {
-                    self.network.start_cloud_p2p()?;
-                }
+                self.ui_on_host_request(port)?;
             }
             _ => {}
         }
@@ -248,6 +294,52 @@ impl<U: Ui> Program<U> {
         Ok(())
     }
 
+    /// The UI is ready to accept data - send them all the data neccessary to fully startup
+    fn ui_on_ready(&mut self) {
+        self.ui.send_message(UiEvents::StartUpText {
+            text: "Starting...".to_string(),
+        });
+
+        self.ui.send_message(UiEvents::InitData {
+            version: std::env::var("CARGO_PKG_VERSION").unwrap(),
+            aircraft: self.definitions_updater.get_all_aircraft_info(),
+        });
+
+        self.ui.send_message(UiEvents::LoadingComplete);
+    }
+
+    /// Connect directly to the server_ip/port if not None, otherwise connect using the session_code provided
+    fn ui_on_join_request(
+        &mut self,
+        username: String,
+        port: Option<u16>,
+        server_ip: Option<String>,
+        session_code: Option<String>,
+    ) -> Result<()> {
+        self.clients.self_client().set_name(username);
+
+        let addr = server_ip
+            .and_then(|ip_string| ip_string.parse::<IpAddr>().ok())
+            .map(|ip| SocketAddr::new(ip, port.expect("should always be sent with ip")));
+
+        if let Some(addr) = addr {
+            self.network.connect_to_address(addr)?
+        } else if let Some(session_code) = session_code {
+        }
+
+        Ok(())
+    }
+
+    /// Host directly if port is None, otherwise request a session_id and begin hosting
+    fn ui_on_host_request(&mut self, port: Option<u16>) -> Result<()> {
+        Ok(if let Some(port) = port {
+            self.network.start_direct(port)?;
+        } else {
+            self.network.start_cloud_p2p()?;
+        })
+    }
+
+    /// The main loop
     pub fn poll(&mut self) -> Result<()> {
         self.process_ui_events()?;
         self.process_network_events()?;
