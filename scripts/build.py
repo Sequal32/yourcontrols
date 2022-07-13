6c314f2044106cf686d2f71a2edef93a9b85ef69
Sequal32 <connor0530@gmail.com>
2022-07-13 19:16:56 -0400

    feat(build): update build script to support relative dirs

--- scripts/build.py
+++ scripts/build.py
@@ -2,19 +2,27 @@ from zipfile import ZipFile, ZIP_DEFLATED
 from os.path import basename
 import os
 
-def insertAllIntoZip(zipObj, directory):
+
+def insert_all_into_zip(zip_obj: ZipFile, directory: str, relative_dir: str = ""):
     # Iterate over all the files in directory
-   for folderName, subfolders, filenames in os.walk(directory):
+    for folder_name, subfolders, filenames in os.walk(directory):
         for filename in filenames:
-            #create complete filepath of file in directory
-            filePath = os.path.join(folderName, filename)
+            # create complete filepath of file in directory
+            file_path = os.path.join(folder_name, filename)
             # Add file to zip
-            zipObj.write(filePath)
+            zip_obj.write(
+                file_path,
+                os.path.join(
+                    relative_dir, os.path.relpath(folder_name, directory), filename
+                ),
+            )
+
 
 zipObj = ZipFile("scripts/out/YourControls.zip", "w", ZIP_DEFLATED)
 
-insertAllIntoZip(zipObj, "definitions/aircraft")
-insertAllIntoZip(zipObj, "definitions/modules")
+insert_all_into_zip(zipObj, "definitions/aircraft", "definitions/aircraft")
+insert_all_into_zip(zipObj, "definitions/modules", "definitions/modules")
+insert_all_into_zip(zipObj, "scripts/build-include", "")
 zipObj.write("assets/logo.png", "assets/logo.png")
 zipObj.write("target/release/YourControls.exe", "YourControls.exe")
 zipObj.write("SimConnect.dll", "SimConnect.dll")
